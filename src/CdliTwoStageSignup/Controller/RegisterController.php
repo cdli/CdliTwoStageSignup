ecba5cf110232b01a8866d25bd6aa90fa005c815
Adam Lundrigan <adamlundrigan@cdli.ca>
2012-06-08 11:58:15 -0230

    ZfcUser\Service\User::createFromForm is now ::register

--- src/CdliTwoStageSignup/Controller/RegisterController.php
+++ src/CdliTwoStageSignup/Controller/RegisterController.php
@@ -71,7 +71,7 @@ class RegisterController extends ActionController
                 // Listen for registration completion and delete the email verification record
                 $service = $this->getEmailVerificationService();
                 $zfcServiceEvents = $locator->get('zfcuser_user_service')->events();
-                $zfcServiceEvents->attach('createFromForm', function($e) use ($service, $model) {
+                $zfcServiceEvents->attach('register', function($e) use ($service, $model) {
                     $service->delete($model);
                 });
 
